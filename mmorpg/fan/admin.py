8256b980f0612d5b2e75d658da11a9f7af71b424
GerulaSerj <118663024+GerulaSerj@users.noreply.github.com>
2023-05-01 20:34:11 +0300

    Add files via upload

--- mmorpg/fan/admin.py
+++ mmorpg/fan/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import CustomUser, Category, Response, Advertisement
+from .models import CustomUser, Category, Response, Advertisement, Subscription
 
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'author')
+    filter_horizontal = ('subscriptions',)
 
-
+admin.site.register(Advertisement, AdvertisementAdmin)
+admin.site.register(Subscription)
 admin.site.register(CustomUser)
 admin.site.register(Category)
 admin.site.register(Response)
-admin.site.register(Advertisement)
--- mmorpg/fan/models.py
+++ mmorpg/fan/models.py
@@ -2,23 +2,46 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, Group, Permission
 from django.utils import timezone
 
-class Category(models.Model): #определяет категории объявлений
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class Category(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
-class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=30, blank=True, null=True)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(default=timezone.now)
-    subscriptions = models.ManyToManyField(Category, blank=True, related_name='users')
 
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, max_length=255)
+    username = models.CharField(max_length=30, blank=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    subscriptions = models.ManyToManyField('Category', blank=True, related_name='users')
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    groups = models.ManyToManyField(Group, blank=True, related_name='users')
+    user_permissions = models.ManyToManyField(Permission, blank=True, related_name='users')
+    objects = CustomUserManager()
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-
-    def __str__(self):
+    def str(self):
         return self.email
 
 class Subscription(models.Model):
@@ -33,7 +56,12 @@ class Subscription(models.Model):
         related_name='category_subscriptions'
     )
 
-class Advertisement (models.Model): #определяет объявления, которые пользователи могут создавать и редактировать
+class Meta:
+    verbose_name = 'CustomUser'
+    verbose_name_plural = 'CustomUsers'
+
+
+class Advertisement(models.Model):
     CATEGORIES = (
         ('TANKS', 'Танки'),
         ('HEALERS', 'Хилы'),
@@ -54,11 +82,14 @@ class Advertisement (models.Model): #определяет объявления, 
     image = models.ImageField(upload_to='advertisement_images/', blank=True, null=True)
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='ads')
     subscriptions = models.ManyToManyField(
-        Subscription,
-        related_name='ads_subscriptions',
-        blank=True,  # делаем поле необязательным
+        'AdSubscription',
+        related_name='advertisements',
+        blank=True,
     )
 
+class AdSubscription(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='ad_subscriptions')
+    category = models.CharField(choices=Advertisement.CATEGORIES, max_length=20)
 class Response(models.Model): #определяет ответы на объявления
     advertisement = models.ForeignKey(Advertisement, on_delete=models.CASCADE, related_name='responses')
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
